e180c840af5ca21c43a1493505fbfb8b225f601c
JayWebDevCom <jaiye.s@gmail.com>
2017-04-11 16:06:30 +0100

    Add ex.9 building a bike and rename files

--- spec/boris_bikes_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'boris_bikes'
-
-describe 'Docking Station' do
-  it 'Docking Station request bike' do
-    docking_station = DockingStation.new
-    expect(docking_station).to respond_to(:release_bike)
-  end
-end
