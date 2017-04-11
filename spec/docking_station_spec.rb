eeb2fbaaeef280e77d8727981515c20dc2dc0bad
JayWebDevCom <jaiye.s@gmail.com>
2017-04-11 18:22:35 +0100

    Add ex.10 kaking docking stations release bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,17 @@
 require 'docking_station'
+require 'bike'
 
 describe 'Docking Station' do
-  it 'Docking Station request bike' do
-    docking_station = DockingStation.new
+
+docking_station = DockingStation.new
+bike = docking_station.release_bike
+
+  it 'responds to request_bike' do
     expect(docking_station).to respond_to(:release_bike)
   end
+
+  it 'provides working bikes' do
+    expect(bike.working?).to eq(true)
+  end
+
 end
